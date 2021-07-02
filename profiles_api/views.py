93e4da13bed6678cd9ef44fa06d4b0da26247df9
ishika261 <budhirajaishika@gmail.com>
2021-07-02 21:59:55 +0530

    created get method for APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methors as function(get,post,patch,put,delete)',
+            'Is similar to a traditional DJango View',
+            'Gives yout he most control over the application logic',
+            'is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
