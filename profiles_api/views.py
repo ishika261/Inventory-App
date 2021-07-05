3806f8da44992f5082ec5bc299b2bc86d489b64b
masterchief410 <shahjainil410@gmail.com>
2021-07-05 16:39:03 +0530

    Added search with name and email

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import authentication
+from rest_framework import filters
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -104,3 +105,5 @@ class UserProfileViewset(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
